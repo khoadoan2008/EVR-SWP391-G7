c15b71816b25add7aaedf1f47b5e5596a5d26f0e
Duoq2805 <142325192+khoadoan2008@users.noreply.github.com>
2025-11-09 00:16:30 +0700

    Fix Deposit

--- EVR/src/main/java/com/group7/evr/repository/DepositRepository.java
+++
@@ -1,9 +0,0 @@
-package com.group7.evr.repository;
-
-import com.group7.evr.entity.Deposit;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepositRepository extends JpaRepository<Deposit,Integer> {
-}
